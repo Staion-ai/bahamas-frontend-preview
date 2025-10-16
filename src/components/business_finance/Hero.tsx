a937252a19552c21970049ea04f96ebcfca44c52
Staion-ai <staioncomco@gmail.com>
2025-10-16 13:34:24 -0500

    FIX paragraph content

--- src/components/business_finance/Hero.tsx
+++ src/components/business_finance/Hero.tsx
@@ -12,7 +12,7 @@ const Hero = () => {
                 <span className="inline-block w-[100%] h-[2px] bg-cyan-300
                                 md:w-[100%]"></span>
 
-                <p className="text-[24px] font-bold md:text-32px">"I want to understand how finance works, and be part of it"</p>
+                <p className="text-[24px] font-bold md:text-32px">&quot;I want to understand how finance works, and be part of it&quot;</p>
             </div>
 
             <div className="w-[80%] h-[45%] flex justify-center items-center
